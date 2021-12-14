70f56b1801df86244df0804d9b685f2ab674f35a
brayanroa <brayanandresrl@ufps.edu.co>
2021-12-13 22:30:06 -0500

    pdf de reportes casi listos

--- src/app/administracion/pages/reportes/reportes-meses/reportes-meses.component.ts
+++ src/app/administracion/pages/reportes/reportes-meses/reportes-meses.component.ts
@@ -6,14 +6,16 @@ import * as global from 'global'
 import { AUTO_STYLE } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
 import { CompraService } from '../../../../services/compra.service';
+import { RouterLinkWithHref } from '@angular/router';
 
 
-type TableRow1 = [number];
+
+
 
 type TableRow = [string, string, number, number, number]
 
 export interface total {
-  total:  number;
+  total: number;
 }
 
 export interface TotalPaquete {
@@ -22,6 +24,16 @@ export interface TotalPaquete {
   total: number;
   porcentajeVentas: number;
   tipoTour: string;
+}
+
+type TableRow2 = [number, number, string, string, number];
+export interface reporteReservas {
+  idReserva: number;
+  totalCompra: number;
+  fecha: string;
+  email: string;
+  cantidadPasajeros: number;
+  estado: null;
 }
 
 @Component({
@@ -33,13 +45,12 @@ export class ReportesMesesComponent implements OnInit {
 
   size: NzButtonSize = 'large';
   dtOptions: DataTables.Settings = {};
-
   dtTrigger = new Subject<any>();
 
   id: any;
   url = `${global.url}/compra/`;
 
-  total: string="";
+  total: string = "";
 
   date = new Date().getMonth();
   agno = new Date().getFullYear();
@@ -60,17 +71,14 @@ export class ReportesMesesComponent implements OnInit {
         url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json"
       }
     };
-
-
-
     this.dtTrigger.next();
   }
 
-  reporteMes(mes: any) {
-    console.log(mes);
-    console.log(this.meses.indexOf(mes) + 1);
 
-  }
+  // reporteMes(mes: any) {
+  //   console.log(mes);
+  //   console.log(this.meses.indexOf(mes) + 1);
+  // }
 
   ngOnDestroy(): void {
     // Do not forget to unsubscribe the event
@@ -89,10 +97,6 @@ export class ReportesMesesComponent implements OnInit {
     pdf.pageMargins([20, 20]); // MARGENES DE LA PÁGINA
     pdf.pageOrientation('landscape'); // 'portrait' --> POSICIÓN DE LA 
 
-    // pdf.header(new Txt(`REPORTE DEL MES DE ${mes.toUpperCase()} - NORTEXPLORADORES`).alignment("center").margin(10).color("green").end) // ENCABEZADO DE LA PÁGINA
-
-
-
     pdf.add(
       new Table([
         [await new Img("https://raw.githubusercontent.com/SantiagoAndresSerrano/img-soka/master/LOGO-01.png").width(80).height(80).noWrap().alignment('center').margin(0).build(),
@@ -102,26 +106,23 @@ export class ReportesMesesComponent implements OnInit {
       ]).widths([140, AUTO_STYLE, 165]).alignment("center").end
     )
 
-   
-
 
     pdf.add(new Txt(`TOTAL VENTAS POR PAQUETES MES DE ${mes.toUpperCase()} DEL AÑO ${this.agno}`).alignment("center").color("blue").bold().margin([0, 30, 0, 0]).end);
 
     pdf.add(this.createTable(data));
 
-    this.obtenerTotal(this.id).subscribe(data=>{
+    this.obtenerTotal(this.id).subscribe(data => {
 
-      this.total=data.ventaMes
+      this.total = data.ventaMes
       console.log(this.total)
-      
+
     })
-    
+
     pdf.add(
       new Table([
-        [ 'Total', new Txt(this.total.toString()).end],
-    ]).widths([ '*', 100 ]).end
+        ['Total', new Txt(this.total.toString()).end],
+      ]).widths(['*', 100]).end
     )
-
 
     pdf.create().open();
   }
@@ -143,7 +144,17 @@ export class ReportesMesesComponent implements OnInit {
   }
 
   extraer(data: TotalPaquete[]): TableRow[] {
-    return data.map(row => [row.nombreTour, row.tipoTour, row.totalPasajeros, row.total, row.porcentajeVentas])
+    // return data.map(row => [row.nombreTour, row.tipoTour, row.totalPasajeros, row.total, row.porcentajeVentas])
+
+    const dat: TableRow[] = data.map(row => [row.nombreTour, row.tipoTour, row.totalPasajeros, row.total, row.porcentajeVentas])
+
+    if (dat.length == 0) {
+      let aux: TableRow[] = [];
+      aux[0] = ["---", "---", 0, 0, 0]
+      return aux
+    } else {
+      return dat
+    }
   }
 
   async datos(): Promise<TotalPaquete[]> {
@@ -151,11 +162,71 @@ export class ReportesMesesComponent implements OnInit {
       .then(resp => resp.json());
   }
 
-
-
-  
-
-  obtenerTotal(mes:any):Observable<any>{
+  obtenerTotal(mes: number): Observable<any> {
+    console.log(mes);
     return this.http.get<any>(`${this.url}${mes}/totalVendidoMes`)
+  }
+
+
+  // TODO: RESERVAS
+
+  async reservas(mes: string) {
+    this.id = this.meses.indexOf(mes) + 1
+
+    const pdf = new PdfMakeWrapper();
+    const data = await this.datosReservas();
+
+    pdf.pageSize("A4"); // TAMAÑO DE LA PÁGINA
+    pdf.pageMargins([20, 20]); // MARGENES DE LA PÁGINA
+    pdf.pageOrientation('landscape'); // 'portrait' --> POSICIÓN DE LA 
+
+    pdf.add(
+      new Table([
+        [await new Img("https://raw.githubusercontent.com/SantiagoAndresSerrano/img-soka/master/LOGO-01.png").width(80).height(80).noWrap().alignment('center').margin(0).build(),
+        new Txt("NORTEXPLORADORES").alignment("center").fontSize(20).margin([0, 25]).bold().end,
+        new Txt("NorteXploradores@gmail.com\n NIT: 1090463941 - 0\n TEL: 3209177920\n RNT: 84950").alignment("center").margin([0, 10]).bold().end],
+
+      ]).widths([140, AUTO_STYLE, 165]).alignment("center").end
+    )
+
+
+    pdf.add(new Txt(`TOTAL RESERVAS EN EL MES DE ${mes.toUpperCase()} DEL AÑO ${this.agno}`).alignment("center").color("blue").bold().margin([0, 30, 0, 0]).end);
+
+    pdf.add(this.createTableReservas(data));
+
+
+    pdf.create().open();
+  }
+
+
+  createTableReservas(data: reporteReservas[]): ITable {
+    return new Table([
+      ["ID RESERVA", "TOTAL COMPRA", "FECHA RESERVA", "CLIENTE", "CANT. PASAJEROS"],
+      ...this.extraerReservas(data)
+    ])
+      .widths([100, 100, 100, AUTO_STYLE, AUTO_STYLE])
+      .layout({
+        fillColor: (rowIndex: number | undefined, node: any | undefined, columnIndex: number | undefined) => {
+          return rowIndex === 0 ? '#D3E4CD' : '#FAEDF0'
+        }
+      }).bold().margin([2, 12]).alignment('center').decorationColor("blue")
+      .end
+  }
+
+  extraerReservas(data: reporteReservas[]): TableRow2[] {
+    const dat: TableRow2[] = data.map(row => [row.idReserva, row.totalCompra, row.fecha, row.email, row.cantidadPasajeros])
+
+    if (dat.length == 0) {
+      let aux: TableRow2[] = [];
+      aux[0] = [0, 0, "---", "---", 0]
+      return aux
+    } else {
+      return dat
+    }
+  }
+
+  async datosReservas(): Promise<reporteReservas[]> {
+    return fetch(`${this.url}${this.id}/totalReservasMesTabla`)
+      .then(resp => resp.json());
   }
 }
